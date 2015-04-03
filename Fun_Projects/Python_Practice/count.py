8810e62af2d01dff0efee0f6cd9534d831aaa186
thefabnab <thefabnab@gmail.com>
2015-04-03 10:09:31 -0400

    Added more python projects

---
+++ Fun_Projects/Python_Practice/count.py
@@ -0,0 +1,7 @@
+#Using a for loop to establish a count
+
+count = 0
+for itervar in [3, 41, 12, 9, 74, 15]:
+    count = count + 1
+    print count 
+print 'Count: ', count
---
+++ Fun_Projects/Python_Practice/firstFunction.py
@@ -0,0 +1,7 @@
+# this is a simple function that ouputs a defined statemement
+
+def print_statement():
+    print 'I can\'t recall what I\'m trying to say but I am trying\n'
+    print 'You know what I mean\n'
+
+print print_statement()
